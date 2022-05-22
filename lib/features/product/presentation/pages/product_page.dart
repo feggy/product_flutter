e689e814e510c0147ba691b7af1c0cbc10e17f2a
feggy <feggy.android@gmail.com>
2022-05-22 12:03:37 +0700

    delete unused library

--- lib/features/product/presentation/pages/product_page.dart
+++ lib/features/product/presentation/pages/product_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:test_lime_commerce/features/product/presentation/widgets/list_product_widget.dart';
 
-class ProductPage extends HookWidget {
+class ProductPage extends StatelessWidget {
   const ProductPage({Key? key}) : super(key: key);
 
   @override
